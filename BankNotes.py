2044890fba77f82775e54562047b08b851c9b617
rishikeshdound <rishikeshdound@gmail.com>
2023-07-11 03:19:10 +0530

    api

---
+++ BankNotes.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: win10
+"""
+from pydantic import BaseModel
+# 2. Class which describes Bank Notes measurements
+class BankNote(BaseModel):
+    variance: float 
+    skewness: float 
+    curtosis: float 
+    entropy: float
